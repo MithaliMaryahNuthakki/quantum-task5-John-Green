7ad6cdc994a6c1133e63f785190c0e71c1820813
MithaliMaryahNuthakki <69067041+MithaliMaryahNuthakki@users.noreply.github.com>
2020-08-22 16:03:12 +0530

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,32 @@
+Project Title- John Green(celebrity)
+
+Languages used:CSS,HTML,PHP
+
+Also used:bootstrap, fontawesome(icons)
+
+HTML section:
+
+<head>
+Initialized all the prequrieddocumentation for jquery,fontawesome 
+Also,include a css file and a FAVICON
+
+</head>
+
+
+<body>
+Contains:
+1.Navbar that is fixed to top even when it is scrolled.(bootsrap)
+2.Three pictures of the author to make good looking header
+3.Details of the Author(celebrity) in a jumbootron(bootsrap)
+4.card-decker(collection of cards)to demonstrate the different books published by the author alonf with the synopsis
+5. It also contains an embeded link to John Greens YoutTube Channel.
+6.Footer:
+	a)Contains related links(blog,awards)
+	b)Adress(psuedo for the time being)
+	c)image of the Author
+	d)Contact details(psuedo for the time being)
+	e)Social media accounts with href(instagram,facebook,twitter)
+	f)copyright.
+</body>
+
+
---
+++ index.php
@@ -0,0 +1,212 @@
+<?php
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="images/favicon.jpg" type="images/jpg" sizes="32x32" border-radius="50%">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>John Green</title>
+	<link rel="stylesheet" type="text/css" href="css.css">
+  </head>
+  <body>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-custom-2 fixed-top ">
+	  <a class="navbar-brand" href="#">John Green</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+		  <li class="nav-item active">
+			<a class="nav-link" href="#about">About <span class="sr-only">(current)</span></a>
+		  </li>
+		  <li class="nav-item active">
+			<a class="nav-link" href="#books-published">Books Published <span class="sr-only">(current)</span></a>
+		  </li>
+		</ul>
+	  </div>
+	</nav>
+	
+	
+	
+	
+	<div class="card top-card mb-3 container" style="max-width: 1200px; margin-top:40px;">
+	  <div class="row no-gutters">
+		<div class="col-md-4">
+		  <img src="images/john2.jpg" class="card-img" alt="...">
+		</div>
+		<div class="col-md-4">
+		  <img src="images/jumbotron.png" class="card-img" alt="...">
+		</div>
+		<div class="col-md-4">
+		  <img src="images/john.jpg" class="card-img" alt="...">
+		</div>
+		
+	  </div>
+	</div>
+	 
+	 <div class="description container" id="about">
+		<h1>John green</h1>
+		<h3> Young Adult Authour</h3>
+		<h5>John Green is the New York Times bestselling author of Looking for Alaska,  An Abundance of Katherines, Paper Towns, The Fault in Our Stars, and Turtles All the Way Down.</h5>
+		<p> Aside from being a novelist, Green is also well known for his YouTube ventures. In 2007, he launched the Vlogbrothers channel with his brother, Hank Green. Since then, John and Hank have launched events such as Project for Awesome and VidCon and created a total of 11 online series, including Crash Course, an educational channel teaching Literature, History, and Science, later joined by fourteen other courses as of 2018</p>
+	</div>
+	 
+	<div class="card-deck" id="books-published">
+	  <div class="card">
+		<img src="images/looking-for-alaska.jpg" class="card-img-top" alt="...">
+		<div class="card-body">
+		  <h5 class="card-title">Looking for Alaska (2005)</h5>
+		  <p class="card-text">Miles Halter is fascinated by famous last words–and tired of his safe life at home. He leaves for boarding school to seek what the dying poet Francois Rabelais called the “Great Perhaps.” Much awaits Miles at Culver Creek, including Alaska Young. Clever, funny, screwed-up, and dead sexy, Alaska will pull Miles into her labyrinth and catapult him into the Great Perhaps.</p>
+		</div>
+	  </div>
+	  <div class="card">
+		<img src="images/an-abundance.jpg" class="card-img-top" alt="...">
+		<div class="card-body">
+		  <h5 class="card-title">An Abundance of Katherines (2006)</h5>
+		  <p class="card-text">When it comes to relationships, everyone has a type. Colin Singleton’s type is girls named Katherine. He has dated–and been dumped by–19 Katherines. In the wake of The K-19 Debacle, Colin–an anagram-obsessed washed-up child prodigy–heads out on a road trip with his overweight, Judge Judy- loving friend Hassan. </p>
+		</div>
+	  </div>
+	  <div class="card">
+		<img src="images/paper-towns.jpg" class="card-img-top" alt="...">
+		<div class="card-body">
+		  <h5 class="card-title">Paper Towns (2008)</h5>
+		  <p class="card-text">Quentin Jacobsen has spent a lifetime loving the magnificently adventurous Margo Roth Spiegelman from afar. So when she cracks open a window and climbs into his life—dressed like a ninja and summoning him for an ingenious campaign of revenge—he follows. After their all-nighter ends, and a new day breaks, Q arrives at school to discover that Margo, always an enigma, has now become a mystery.</p>
+		</div>
+	  </div>
+
+	  <div class="card">
+		<img src="images/will-grayson.jpg" class="card-img-top" alt="...">
+		<div class="card-body">
+		  <h5 class="card-title">Will Grayson, Will Grayson(2010)</h5>
+		  <h6 class="card-title"> – with David Levithan </h6>
+		  <p class="card-text">Will Grayson meets Will Grayson. One cold night, in a most unlikely corner of Chicago, two strangers are about to cross paths. From that moment on, their world will collide and lives intertwine.</P>
+		</div>
+	  </div>
+	  <div class="card">
+		<img src="images/tfios.jpg" class="card-img-top" alt="...">
+		<div class="card-body">
+		  <h5 class="card-title">The Fault in Our Stars (2012)</h5>
+		  <p class="card-text">Despite the tumor-shrinking medical miracle that has bought her a few years, Hazel has never been anything but terminal, her final chapter inscribed upon diagnosis. But when a gorgeous plot twist named Augustus Waters suddenly appears at Cancer Kid Support Group, Hazel's story is about to be completely rewritten.</p>
+		</div>
+	  </div>
+	  <div class="card">
+		<img src="images/turtle-all-the-way-down.jpg" class="card-img-top" alt="...">
+		<div class="card-body">
+		  <h5 class="card-title">Turtles All the Way Down (2017)</h5>
+		  <p class="card-text">It all begins with a fugitive billionaire and the promise of a cash reward. Turtles All the Way Down is about lifelong friendship, the intimacy of an unexpected reunion, Star Wars fan fiction, and tuatara. But at its heart is Aza Holmes, a young woman navigating daily existence within the ever-tightening spiral of her own thoughts.
+</p>
+		  
+		</div>
+	  </div>
+	</div>
+	
+	<div class="youtube">
+	<h4>YouTube Channel</h4>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/Sc850jqMcwo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+	
+	
+<footer class="page-footer font-small bg-custom-2 pt-4">
+ <div class="container text-center text-md-left">
+    <div class="row">
+
+      <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+      <img src="images/footer.jpg" height="200px">
+
+      </div>
+     <hr class="clearfix w-100 d-md-none">
+     <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+       
+        <h5 class="font-weight-bold text-uppercase mb-4">Useful Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <a href="#books-published">Books Publishd</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="#about">ABOUT</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="http://www.johngreenbooks.com/blog/2016/6/25/blog-title-entry-goes-here">BLOG</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="https://www.imdb.com/name/nm1981261/awards">AWARDS</a>
+            </p>
+          </li>
+        </ul>
+
+      </div>
+	  <hr class="clearfix w-100 d-md-none">
+	<div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        
+        <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <i class="fa fa-home" aria-hidden="true"></i> New York, NY 10012, US</p>
+          </li>
+          <li>
+            <p>
+             <i class="fa fa-envelope" aria-hidden="true"></i> info@example.com</p>
+          </li>
+          <li>
+            <p>
+             <i class="fa fa-phone-square" aria-hidden="true"></i> + 01 234 567 88</p>
+          </li>
+          </ul>
+
+      </div>
+      <hr class="clearfix w-100 d-md-none">
+	<div class="col-md-2 col-lg-2 text-center mx-auto my-4">
+		<h5 class="font-weight-bold text-uppercase mb-4">Follow</h5>
+	<p>
+	<a type="button" class="btn-floating btn-fb"href="https://www.facebook.com/JohnGreenfans/">
+          <i class="fa fa-facebook-official fa-3x" aria-hidden="true"></i>
+        </a>
+	</p>
+	<p>
+        <a type="button" class="btn-floating btn-tw"href="https://www.instagram.com/johngreenwritesbooks/?hl=es">
+          <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+		</a>
+    </p> 
+	<p>
+        <a type="button" class="btn-floating btn-gplus"href="https://twitter.com/johngreen?lang=en">
+          <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+		</a>
+    </p>   
+        </div>
+      
+
+    </div>
+   </div>
+ <div class="footer-copyright text-center py-3">© 2020 Copyright: Mithali
+</div>
+
+</footer>
+
+  
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
